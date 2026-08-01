e4d5241e389d71e11939ae6bc226efe2540b4c32
maide özdemir <maideozdemir96@gmail.com>
2026-08-01 18:26:02 +0300

    Remove legacy keyword search

--- embeddings.py
+++ embeddings.py
@@ -3,8 +3,6 @@ import math
 from foundry_local_sdk import Configuration, FoundryLocalManager
 from foundry_local_sdk.exception import FoundryLocalException
 
-from search import clean_text
-
 
 _manager_initialized = False
 
@@ -83,31 +81,3 @@ def calculate_cosine_similarity(embedding1, embedding2):
         return 0.0
 
     return dot_product / (magnitude1 * magnitude2)
-
-
-# Temporary old embedding functions.
-# These will be removed after the real vector search is connected successfully.
-
-def create_simple_embedding(text):
-    clean = clean_text(text)
-    words = clean.split()
-
-    embedding = {}
-
-    for word in words:
-        if word in embedding:
-            embedding[word] += 1
-        else:
-            embedding[word] = 1
-
-    return embedding
-
-
-def calculate_similarity(embedding1, embedding2):
-    score = 0
-
-    for word in embedding1:
-        if word in embedding2:
-            score += min(embedding1[word], embedding2[word])
-
-    return score
